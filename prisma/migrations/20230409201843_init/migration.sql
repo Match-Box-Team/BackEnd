16dfb536293228583ce2747013125142de7db546
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-10 05:20:11 +0900

    chore: migration

--- prisma/migrations/20230409201843_init/migration.sql
+++ prisma/migrations/20230409201843_init/migration.sql
@@ -63,7 +63,7 @@ CREATE TABLE "Friend" (
 CREATE TABLE "Channel" (
     "channel_id" UUID NOT NULL,
     "channel_name" TEXT NOT NULL,
-    "password" TEXT,
+    "password" TEXT NOT NULL,
     "count" INTEGER NOT NULL,
     "is_public" BOOLEAN NOT NULL,
     "is_dm" BOOLEAN NOT NULL,

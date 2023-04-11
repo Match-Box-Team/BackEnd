dfad1f8788419e07bd4ea847bfaf223ef9c37359
Jinho Kim <rlawlsgh8113@naver.com>
2023-04-11 15:48:17 +0900

    Revert "13 feature 개발용 db 폴더 추가 makefile 추가"

--- src/games/games.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('games')
-export class GamesController {}
--- src/games/games.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GamesController } from './games.controller';
-import { GamesService } from './games.service';
-
-@Module({
-  controllers: [GamesController],
-  providers: [GamesService]
-})
-export class GamesModule {}

d78547bb5d8313fca9047c02452793bd56182956
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-12 22:22:41 +0900

    feat: 소켓으로 유저(online, offline) 상태 업데이트 구현

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -3,28 +3,23 @@ import { AccountService } from './account.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from '@prisma/client';
 import { VerifyCodeDto } from './dto/verify-code.dto';
+import { stringify } from 'querystring';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
-  // @Get()
-  // async getUser() : Promise<User[]> {
-  //     return this.accountService.getUsers();
-  // }
+  @Get()
+  async getUser(): Promise<User[]> {
+    return this.accountService.getUsers();
+  }
 
   // @Patch(':userId')
-  // async updateUser(
+  // async updateUserProfile(
   //   @Param('userId') userId: string,
   //   @Body() updateUserDto: UpdateUserDto,
   // ) {
-  //   return this.accountService.updateUser({
-  //     where: { userId: userId },
-  //     data: {
-  //         nickname: updateUserDto.nickname,
-  //         image: updateUserDto.image,
-  //     },
-  //   })
+  //   return this.accountService.updateUserProfile(userId, updateUserDto);
   // }
 
   // @Post()

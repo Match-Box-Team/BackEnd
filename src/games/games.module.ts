4582aecf7c9483e66caaba21bbc4587bb890a723
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-14 16:30:50 +0900

    feat: provider 추가

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -3,9 +3,19 @@ import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
 import { PrismaService } from 'prisma/prisma.service';
 import { GamesRepository } from './repository/games.repository';
+import { GameEventsGateway } from './events/game.gateway';
+import { AccountService } from 'src/account/account.service';
+import { AccountRepository } from 'src/account/repository/account.repository';
 
 @Module({
   controllers: [GamesController],
-  providers: [GamesService, PrismaService, GamesRepository],
+  providers: [
+    GamesService,
+    PrismaService,
+    AccountService,
+    AccountRepository,
+    GamesRepository,
+    GameEventsGateway,
+  ],
 })
 export class GamesModule {}

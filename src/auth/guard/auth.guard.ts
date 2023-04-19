05c060913f2e7c80e2a2c1a8ea5a024c1413e1ce
cofls980 <qawse980@gmail.com>
2023-04-19 17:17:58 +0900

    refactor: channel의 소켓 통신에 useguard 추가

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -5,33 +5,58 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
 // import { jwtConstants } from './constants';
 import { Request } from 'express';
+import { Socket } from 'socket.io';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+    if (context.getType() === 'http') {
+      const request = context.switchToHttp().getRequest();
+      const token = this.extractToken(request.headers.authorization);
 
-    if (!token) {
-      throw new UnauthorizedException();
-    }
-    try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_SECRET,
-      });
-      request['id'] = payload;
-    } catch {
-      throw new UnauthorizedException();
+      if (!token) {
+        throw new UnauthorizedException();
+      }
+      try {
+        const payload = await this.jwtService.verifyAsync(token, {
+          secret: process.env.JWT_SECRET,
+        });
+        request['id'] = payload;
+      } catch {
+        throw new UnauthorizedException();
+      }
+    } else if (context.getType() === 'ws') {
+      const client: Socket = context.switchToWs().getClient();
+      const token = this.extractToken(client.handshake.headers.authorization);
+
+      if (!token) {
+        client.emit('error', {
+          UnauthorizedException: 'Unauthorized access when socket connection',
+        });
+        return false;
+      }
+      try {
+        const payload = await this.jwtService.verifyAsync(token, {
+          secret: process.env.JWT_SECRET,
+        });
+        client.data.user = payload;
+      } catch (error) {
+        client.emit('error', {
+          UnauthorizedException: 'Unauthorized access when socket connection',
+        });
+        return false;
+      }
     }
     return true;
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+  private extractToken(authorization: string): string | undefined {
+    const [type, token] = authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
 }

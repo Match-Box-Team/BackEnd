e5533174195bef352eb1f8f65391b79229c209ce
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-30 02:12:17 +0900

    feat: 특정 유저 게임 초대

--- src/account/events/account.gateway.ts
+++ src/account/events/account.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger, UseGuards } from '@nestjs/common';
+import { ConflictException, Logger, NotFoundException, UseGuards } from '@nestjs/common';
 import {
   ConnectedSocket,
   OnGatewayConnection,
@@ -11,6 +11,7 @@ import {
 import { Server, Socket } from 'socket.io';
 import { AccountService } from '../account.service';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { User } from '@prisma/client';
 
 @UseGuards(AuthGuard)
 @WebSocketGateway({ cors: true })
@@ -52,12 +53,35 @@ export class AccountEventsGateway
       const userId = client.data.user['id'];
       const user = await this.accountService.getUser(userId);
       console.log(`login name: ${user.nickname} --- id: ${userId}`);
-      client.data.userId = userId;
       await this.accountService.updateUserState(userId, 'online');
+      client.data.userInfo = {
+        ...user,
+        status: 'online',
+      }
     } catch {
       client.emit('error', {
         NotFoundException: 'Not found user',
       });
     }
+  }
+
+  // 게임 초대
+  @SubscribeMessage('game')
+  async game(client: Socket, enemy: { userId: string }) {
+    if (client.data.userInfo.userId === enemy.userId) {
+      // 아래 3줄 나중에 지워야함
+      const user: User = client.data.userInfo;
+      client.emit('game', user);
+      return;
+      // throw new ConflictException("자기 자신에게 게임을 신청했습니다")
+    }
+    const clients = this.server.sockets.sockets;
+    const matchedUser = Array.from(clients.values())
+      .filter(user => user.data.userInfo.userId === enemy.userId);
+    if (!matchedUser) {
+      throw new NotFoundException("상대가 로그인 상태가 아닙니다");
+    }
+    console.log("상대 발견!\n 상대 소켓 id:", matchedUser[0].id);
+    client.to(matchedUser[0].id).emit('game', client.data.userInfo);
   }
 }
